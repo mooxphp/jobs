b12ee56ed0c8e0113133d4312196e72d84ffa773
Igor clauss <mail@igorclauss.de>
2023-12-04 09:26:04 +0100

    german translation added

---
+++ resources/lang/de/translations.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'breadcrumb' => 'Tasks',
+    'title' => 'Tasks',
+    'navigation_label' => 'Tasks',
+    'navigation_group' => 'Task Manager',
+    'total_jobs' => 'Anzahl ausgeführter Tasks',
+    'waiting_jobs' => 'Anzahl wartender Tasks',
+    'execution_time' => 'Gesamt Ausführungszeit',
+    'average_time' => 'Durchschnittliche Ausführungszeit',
+    'succeeded' => 'Erfolgreich',
+    'failed' => 'Fehlgeschlagen',
+    'running' => 'In Bearbeitung',
+    'waiting' => 'Wartend',
+    'status' => 'Status',
+    'attempts' => 'Versuche',
+    'name' => 'Name',
+    'queue' => 'Warteschlange',
+    'progress' => 'Fortschritt',
+    'started_at' => 'Gestartet am',
+    'created_at' => 'Erstellt am',
+];
